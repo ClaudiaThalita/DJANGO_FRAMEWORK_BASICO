97c56b7cf6c88fc790a00109b377a5836048f508
ClaudiaThalita <claudiathalitamendes@outlook.com>
2021-12-05 21:18:47 -0300

    feature: Aula 14- Apresentando dados do banco de dados no template

--- django1/core/views.py
+++ django1/core/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+from core.models import Produto
+
 def index(request):
-    return render(request, 'index.html')
+    produtos = Produto.objects.all()
+
+    context = {
+        'produtos': produtos
+    }
+
+    return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    print(f' PK: {pk}')
+    return render( request, 'produto.html')
